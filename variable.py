2eb1db8cbc881bba98507de10ba3ba5ad5d79c1e
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-14 19:39:34 +0500

    Variables

---
+++ variable.py
@@ -0,0 +1,11 @@
+x = 5
+y = "Raffay"
+print(x)
+print(y)
+name = "Jhon"
+print(name)
+import keyword
+print("Python keywords are \n")
+print(keyword.kwlist)
+x = input("Enter your name: ")
+print("\n Hello!", x, "\n Welcome to programming")
