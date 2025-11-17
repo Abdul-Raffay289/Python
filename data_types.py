8179f24dcd933200fce40836e1f12d55c10c1f53
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-17 19:29:02 +0500

    Data Types in python

---
+++ data_types.py
@@ -0,0 +1,28 @@
+a = 8
+b = "Jhon"
+c = 10.8
+d = True
+e = False
+f = 'a'
+print("Type of a is ",type(a))
+print("Type of b is ",type(b))
+print("Type of c is ",type(c))
+print("Type of d is ",type(d))
+print("Type of e is ",type(e))
+print("Type of f is ",type(f))
+# Typecasting
+name = "Raffay"
+age = 10
+weight = 30.7
+is_student = True
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+weight = int(weight)
+print("Type of weight is ",type(weight))
+age = str(age)
+print("Type of age is ",type(age))
+# reverse string
+text = str(input("Enter the text:"))
+revtext = text[::-1]
+print("The reverse text is: ",revtext)
