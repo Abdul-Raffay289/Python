119a73669151e68c6720e147def3149c3d204b55
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-19 19:11:29 +0500

    counting the notes

---
+++ Python Operators/Sorting.py
@@ -0,0 +1,12 @@
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 96
+tree_5 = 11
+#finding sum
+sum = tree_1 + tree_2 + tree_3 + tree_4 + tree_5
+print("The sum of all trees are: ", sum)
+#finding average
+#formula of average: sum of all given numbers / total numbers
+average = sum / 5
+print("The average of all trees is: ", average)
