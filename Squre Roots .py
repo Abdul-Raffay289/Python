692046f92b018952b393dd320399f0624d196e0d
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-22 09:55:12 +0500

    Square Roots

---
+++ Squre Roots .py
@@ -0,0 +1,12 @@
+root_1 = 40
+root_2 = 48
+root_3 = 61
+root_4 = 93
+root_5 = 67
+#finding sum
+sum = root_1 + root_2 + root_3 + root_4 + root_5
+print("The sum of all trees are: ", sum)
+#finding average
+#formula of average: sum of all given numbers / total numbers
+average = sum / 5
+print("The average of all root is: ", average)
