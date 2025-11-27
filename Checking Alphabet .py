7b9e0a7a9469bb2c73f2517ea49c0f482dadce7c
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-27 19:08:39 +0500

    Checking the alphapet

---
+++ Checking Alphabet .py
@@ -0,0 +1,7 @@
+# Checking The alphabet
+x = input("Write the alphabets: ") 
+alphabet = ("a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z")
+if x in alphabet:
+    print("yes this is an alphabet")
+else:
+    print("This is not alphabet")
