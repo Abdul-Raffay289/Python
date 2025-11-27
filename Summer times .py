33277fcf983901ab9b937897eb962b2ceca0d97a
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-11-27 18:43:05 +0500

    What's The Tmperature

---
+++ Summer times .py
@@ -0,0 +1,8 @@
+# Program to measure temperature using conditional statements
+temp = float(input("Enter the temperature: "))
+if temp > 20:
+    print("Yau can wear warm clothes")
+elif temp < 20:
+    print("You can wear light clothes")
+else:
+    print("You can on AC")
