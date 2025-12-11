c45189ec6de37fa15dceb4b3ff6379a92c8e6859
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-12-11 18:51:25 +0500

    Right mirrored - angled triangle

---
+++ Right mirrored-angled triangle .py
@@ -0,0 +1,7 @@
+rows = int(input("Enter number of rows: "))
+for i in range(1, rows + 1):
+    for j in range(rows - i):
+        print(" ", end="")
+    for k in range(1, i + 1):
+        print(k, end="")
+    print()
