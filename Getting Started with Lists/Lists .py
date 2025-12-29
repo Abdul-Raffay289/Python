9781eaad7923ccc6fb1f6627428449e48a59e7de
Abdul-Raffay289 <abdullraffay289@gmail.com>
2025-12-29 19:26:23 +0500

    Getting Started with Listed

---
+++ Getting Started with Lists/Lists .py
@@ -0,0 +1,9 @@
+empty_list = []
+print()
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(number)
+triple = [2, 3, 4]*3
+print(triple)
+reverse_list = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+reverse_list = reverse_list[:: - 1]
+print(reverse_list)
