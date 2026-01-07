b9481afd39cca51410995712b122a9297e2beab2
Abdul-Raffay289 <abdullraffay289@gmail.com>
2026-01-07 19:07:35 +0500

    Sets and array

---
+++ Sets and Arrays/Operations on Sets .py
@@ -0,0 +1,6 @@
+my_set = {1, 2, 3}
+print(my_set)
+my_set1 = {1.0, "Hello", (1, 2, 3)}
+print(my_set1)
+my_set2 = set([1, 2, 3, 2])
+print(my_set2)
---
+++ Sets and Arrays/Set Intersection .py
@@ -0,0 +1,10 @@
+setx = {"Green", "Blue"}
+sety = {"Blue", "Yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\n Intersection of two sets:")
+setz = setx.intersection(sety)
+print(setz)
+seta = setx.union(sety)
+print(seta)
