6fb97dafe7680a8f1c2acd69316542fabe861d45
Abdul-Raffay289 <abdullraffay289@gmail.com>
2026-01-21 19:10:44 +0500

    Encapsulation

---
+++ Encapsulation and Special Functions/Keep It Private .py
@@ -0,0 +1,9 @@
+class My_Class:
+    __PrivateVar = 27
+    def __Privmith(self):
+        print("I'm inside class My_Class")
+    def Hello(self):
+        print("Private variable value:", My_Class.__PrivateVar)
+obj = My_Class()
+obj.Hello()
+obj. __Privmith
