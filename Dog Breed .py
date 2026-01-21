b3ecd99a89ed3af290615c3d2905c78bcc509b05
Abdul-Raffay289 <abdullraffay289@gmail.com>
2026-01-21 20:21:39 +0500

    Dog Breed

---
+++ Dog Breed .py
@@ -0,0 +1,9 @@
+class Dog:
+    def __init__(self, name, Feed):
+        self.name = name
+        self.Feed = Feed
+    def Hello(self):
+        print("Hi I am a dog")
+obj = Dog(Tommy, Bone)
+obj.Hello
+obj.display()
